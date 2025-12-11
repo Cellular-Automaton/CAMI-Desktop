ffdc062da62d8f7ecdf049d6ff578f0e7b2a1fcb
Louis Bassagal <91676343+LouisBassagal@users.noreply.github.com>
2025-12-11 11:04:59 +0100

    [ADD] Add color change to setting bar

--- src/components/SettingBar/SettingBar.jsx
+++ src/components/SettingBar/SettingBar.jsx
@@ -2,11 +2,9 @@ import React from "react";
 import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
 import CropSquareRoundedIcon from '@mui/icons-material/CropSquareRounded';
 import MinimizeRoundedIcon from '@mui/icons-material/MinimizeRounded';
-import ArrowBackIosRoundedIcon from '@mui/icons-material/ArrowBackIosRounded';
-import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded';
+import { ThemeManagerInstance } from "../../utils/Themes.jsx";
 
 const SettingBar = () => {
-
     function handleClose() {
         window.electron.onCloseApp();
     }
@@ -22,15 +20,15 @@ const SettingBar = () => {
     return (
         <div className="flex fixed -top-1 w-full ml-16 pr-16 h-10 z-20 justify-end items-center drag">
             <div className="flex flex-row justify-center items-center no-drag">
-                <button className="text-white hover:bg-white/20 h-full w-12 p-1 no-drag" onClick={handleMinimize}>
+                <button className="hover:bg-gray-400/70 h-full w-12 p-1 no-drag" onClick={handleMinimize}>
                     <MinimizeRoundedIcon sx={{ color: "var(--color-text)", fontSize: 16 }} />
                 </button>
 
-                <button className="text-white hover:bg-white/20 h-full w-12 p-1 no-drag" onClick={handleMaximize}>
+                <button className="hover:bg-gray-400/70 h-full w-12 p-1 no-drag" onClick={handleMaximize}>
                     <CropSquareRoundedIcon sx={{ color: "var(--color-text)", fontSize: 16 }} />
                 </button>
 
-                <button className="text-white hover:bg-red-600/80 h-full w-12 p-1 no-drag" onClick={handleClose}>
+                <button className="hover:bg-red-600/80 h-full w-12 p-1 no-drag" onClick={handleClose}>
                     <CloseRoundedIcon sx={{ color: "var(--color-text)", fontSize: 16 }} />
                 </button>
             </div>
