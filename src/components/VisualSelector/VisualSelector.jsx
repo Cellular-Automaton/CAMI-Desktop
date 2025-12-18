3e79ab6df3a3ab2c22790e628aa282e205b12d93
Louis Bassagal <91676343+LouisBassagal@users.noreply.github.com>
2025-12-18 16:42:14 +0100

    [FIX] Notify user if no visual available for an algorithm

--- src/components/VisualSelector/VisualSelector.jsx
+++ src/components/VisualSelector/VisualSelector.jsx
@@ -52,63 +52,69 @@ const VisualSelector = ({ algorithmId, selectedVisual, onSelect }) => {
         <div id="visual-container" className="bg-background h-full text-text px-5 py-5 flex flex-col overflow-hidden font-mono">
             <label htmlFor="visual-select" className="text-xl font-bold">Visuals available:</label>
             <p className="mb-5 text-textAlt">Select a visual you want to use.</p>
-            <div id="visual-select" className="flex flex-col gap-5 overflow-y-scroll px-10 py-5 max-h-96">
-                {
-                    currentVisual.map((vis) => {
-                        return (
-                        <Card
-                            key={vis.id}
-                            className="
-                                relative flex flex-row items-center h-fit w-full shrink-0
-                                cursor-pointer hover:opacity-50 !bg-backgroundAlt
-                                hover:ring-4 hover:ring-primary hover:scale-105 !transition-all duration-200
-                                focus-within:opacity-50 focus-within:ring-4 focus-within:ring-primary
-                                focus-within:scale-105
-                                "
-                            variant={selectedVisual && selectedVisual.id === vis.id ? "elevation" : "outlined"}
-                            onClick={() => onSelect(vis)}
-                    >
-                        <CardActionArea>
-                            <div className="p-4 h-full w-full flex flex-row items-center gap-5">
-                                <div className="flex flex-col w-3/5 h-full justify-center">
-                                    <h1 className="!text-text">{vis.name}</h1>
-                                    <h2 className="!text-textAlt text-sm">Made by: {vis.posted_by.username === null ? "Unknown" : vis.posted_by.username}</h2>
-                                </div>
-                                <div className="flex flex-col h-full w-1/5 justify-center">
-                                    {
-                                        vis.isInstalled === undefined ? (
-                                            <Chip label="Loading..." color="warning"></Chip>
-                                        ) : vis.isInstalled ? (
-                                            <Chip label="Installed" color="success"></Chip>
-                                        ) : (
-                                            <Chip label="Not Installed" color="error"></Chip>
-                                        )
-                                    }
-                                </div>
-                                <div className="flex flex-col h-full w-1/5 justify-center items-center">
-                                    {
-                                        vis.isInstalled ? (
-                                        <Button
-                                            variant="outlined"
-                                            color="error"
-                                            onClick={async (e) => {
-                                                e.stopPropagation();
-                                                await handleDeleteVisual(vis);
-                                            }}
-                                        >
-                                            Uninstall
-                                        </Button>
-                                        ) : 
-                                        null
-                                    }
-                                </div>
-                            </div>
-                        </CardActionArea>
-                    </Card>
-                        )
-                    })
-                }
-            </div>
+            {
+                currentVisual.length === 0 ? (
+                    <p className="text-textAlt">No visuals available for this algorithm.</p>
+                ) : (
+                    <div id="visual-select" className="flex flex-col gap-5 overflow-y-scroll px-10 py-5 max-h-96">
+                        {
+                            currentVisual.map((vis) => {
+                                return (
+                                <Card
+                                    key={vis.id}
+                                    className="
+                                        relative flex flex-row items-center h-fit w-full shrink-0
+                                        cursor-pointer hover:opacity-50 !bg-backgroundAlt
+                                        hover:ring-4 hover:ring-primary hover:scale-105 !transition-all duration-200
+                                        focus-within:opacity-50 focus-within:ring-4 focus-within:ring-primary
+                                        focus-within:scale-105
+                                        "
+                                    variant={selectedVisual && selectedVisual.id === vis.id ? "elevation" : "outlined"}
+                                    onClick={() => onSelect(vis)}
+                            >
+                                <CardActionArea>
+                                    <div className="p-4 h-full w-full flex flex-row items-center gap-5">
+                                        <div className="flex flex-col w-3/5 h-full justify-center">
+                                            <h1 className="!text-text">{vis.name}</h1>
+                                            <h2 className="!text-textAlt text-sm">Made by: {vis.posted_by.username === null ? "Unknown" : vis.posted_by.username}</h2>
+                                        </div>
+                                        <div className="flex flex-col h-full w-1/5 justify-center">
+                                            {
+                                                vis.isInstalled === undefined ? (
+                                                    <Chip label="Loading..." color="warning"></Chip>
+                                                ) : vis.isInstalled ? (
+                                                    <Chip label="Installed" color="success"></Chip>
+                                                ) : (
+                                                    <Chip label="Not Installed" color="error"></Chip>
+                                                )
+                                            }
+                                        </div>
+                                        <div className="flex flex-col h-full w-1/5 justify-center items-center">
+                                            {
+                                                vis.isInstalled ? (
+                                                <Button
+                                                    variant="outlined"
+                                                    color="error"
+                                                    onClick={async (e) => {
+                                                        e.stopPropagation();
+                                                        await handleDeleteVisual(vis);
+                                                    }}
+                                                >
+                                                    Uninstall
+                                                </Button>
+                                                ) : 
+                                                null
+                                            }
+                                        </div>
+                                    </div>
+                                </CardActionArea>
+                            </Card>
+                                )
+                            })
+                        }
+                    </div>
+                )
+            }
         </div>
     );
 }
