0f3e2a813df4b880633a167ce421758f85066836
Louis Bassagal <91676343+LouisBassagal@users.noreply.github.com>
2025-12-18 12:29:27 +0100

    [FIX] Change admin page for better UI/UX

--- src/pages/Admin/AdminPage.jsx
+++ src/pages/Admin/AdminPage.jsx
@@ -13,6 +13,7 @@ export default function AdminPage() {
     const [fiveLastUsers, setFiveLastUsers] = useState([]);
     const [fiveLastAlgorithms, setFiveLastAlgorithms] = useState([]);
     const [fiveLastComments, setFiveLastComments] = useState([]);
+    const [fiveLastVisuals, setFiveLastVisuals] = useState([]);
     const { getLastestUsers, getLastestAlgorithms, getLastestComments } = useContext(APIContext);
 
     useEffect(() => {
@@ -28,9 +29,7 @@ export default function AdminPage() {
         });
 
         getLastestAlgorithms().then(fetchedAlgorithms => {
-            
             setFiveLastAlgorithms(fetchedAlgorithms);
-            
         }).catch(err => {
             console.error("Failed to fetch last 5 algorithms:", err);
         });
@@ -75,77 +74,114 @@ export default function AdminPage() {
     }
 
     return (
-        <div className="bg-midnight h-full w-full overflow-hidden font-mono py-10 px-5 justify-center items-center flex flex-col">
-            <h1 className="text-3xl font-bold text-white mb-4">Admin Page</h1>
-            <p className="text-white">Welcome to the admin page. Here you can manage the application settings and user accounts.</p>
+        <div className="bg-background h-full w-full overflow-hidden font-mono py-10 px-5 justify-center items-center flex flex-col">
+            <div>
+                <h1 className="text-3xl font-bold text-text mb-4">Admin Page</h1>
+                <p className="text-textAlt">Welcome to the admin page. Here you can manage the application settings and user accounts.</p>
+            </div>
 
-            <div id="admin-select-controls" className="mt-10 flex flex-row gap-4 w-full h-full">
-                <button className="flex flex-col p-5 w-full h-full shadow-sm shadow-primary text-white rounded-lg"
+            <div id="admin-select-container" className="flex flex-col mt-10 py-10 px-10 gap-6 w-full h-full overflow-y-scroll">
+
+                <button className="flex flex-col bg-backgroundAlt p-5 w-full h-fit 
+                    text-text rounded-lg hover:scale-105 hover:ring-4 hover:ring-primary
+                    focus:scale-105 focus:ring-4 focus:ring-primary transition-all duration-200 cursor-pointer" 
                     onClick={() => openAdminPopup("user")}>
                     <h2 className="text-lg mb-2 font-bold">User Management</h2>
                     <span className="text-sm opacity-70">Manage user accounts and permissions</span>
                     <Divider sx={{ my: 1, backgroundColor: 'white', height: '1px' }} flexItem className="!my-5" />
                     <span className="text-sm opacity-70">Last users :</span>
+                    <div id="last-users" className="flex flex-row overflow-hidden">
 
-                    <div className="flex flex-col overflow-y-auto h-full w-full px-5 mt-5 gap-5">
                         {
                             fiveLastUsers === undefined || fiveLastUsers.length === 0 ? (
-                                <p className="flex flex-col justify-center text-white h-full items-center">No users found.</p>
+                                <p className="flex flex-col justify-center text-text h-full items-center">No users found.</p>
                             ) : (
                                 fiveLastUsers.map((user) => (
-                                    <Card key={user.user_id} sx={{ height: '100px' }} className="flex flex-row justify-between items-center !bg-midnight-opacity !text-white">
-                                        <CardContent className="flex flex-col items-start text-left">
-                                            <p className="text-lg font-bold w-full">{user.username}</p>
-                                            <p className="text-sm opacity-70 w-full">{user.user_id}</p>
+                                    <Card key={user.user_id} sx={{ height: 'fit-content', minWidth: '200px' }} className="flex flex-col justify-between items-start !bg-background !text-text m-2 p-2">
+                                        <CardContent className="flex flex-col items-start text-left w-full">
+                                            <p className="text-lg text-text font-bold w-full">{user.username}</p>
+                                            <p className="text-textAlt text-sm mb-2">{formatDistance(new Date(user.created_at), new Date(), { addSuffix: true })}</p>
+                                            <p className="text-xs text-textAlt w-full">{user.user_id}</p>
                                         </CardContent>
                                     </Card>
                                 ))
                             )
                         }
+
                     </div>
                 </button>
-                <button className="flex flex-col p-5 w-full h-full shadow-sm shadow-primary text-white rounded-lg"
+
+                <button className="flex flex-col bg-backgroundAlt p-5 w-full h-fit 
+                    text-text rounded-lg hover:scale-105 hover:ring-4 hover:ring-primary
+                    focus:scale-105 focus:ring-4 focus:ring-primary transition-all duration-200 cursor-pointer" 
                     onClick={() => openAdminPopup("algorithm")}>
                     <h2 className="text-lg mb-2 font-bold">Algorithm Management</h2>
                     <span className="text-sm opacity-70">Manage algorithms and their configurations</span>
                     <Divider sx={{ my: 1, backgroundColor: 'white', height: '1px' }} flexItem className="!my-5" />
                     <span className="text-sm opacity-70">Last algorithms :</span>
-
-                    <div className="flex flex-col overflow-y-auto h-full w-full px-5 mt-5 gap-5">
+                    <div id="last-algorithms" className="flex flex-row overflow-hidden">
                         {
                             fiveLastAlgorithms === undefined || fiveLastAlgorithms.length === 0 ? (
-                                <p className="flex flex-col justify-center text-white h-full items-center">No algorithms found.</p>
+                                <p className="flex flex-col justify-center text-text h-full items-center">No algorithms found.</p>
                             ) : (
                                 fiveLastAlgorithms.map((algorithm) => (
-                                    <Card key={algorithm.automaton_id} sx={{ height: '100px' }} className="flex flex-row justify-between items-center !bg-midnight-opacity !text-white">
-                                        <CardContent className="flex flex-col items-start text-left">
-                                            <p className="text-lg font-bold w-full">{algorithm.name}</p>
-                                            <p className="text-sm opacity-70 w-full">{algorithm.automaton_id}</p>
+                                    <Card key={algorithm.automaton_id} sx={{ height: 'fit-content', minWidth: '200px' }} className="flex flex-col justify-between items-start !bg-background !text-text m-2 p-2">
+                                        <CardContent className="flex flex-col items-start text-left w-full">
+                                            <p className="text-lg text-text font-bold w-full">{algorithm.name}</p>
+                                            <p className="text-sm text-textAlt mb-2 w-full">{algorithm.automaton_id}</p>
                                         </CardContent>
                                     </Card>
                                 ))
                             )
                         }
                     </div>
-
                 </button>
-                <button className="flex flex-col p-5 w-full h-full shadow-sm shadow-primary text-white rounded-lg"
+
+                <button className="flex flex-col bg-backgroundAlt p-5 w-full h-fit 
+                    text-text rounded-lg hover:scale-105 hover:ring-4 hover:ring-primary
+                    focus:scale-105 focus:ring-4 focus:ring-primary transition-all duration-200 cursor-pointer" 
+                    onClick={() => openAdminPopup("visual")}>
+                    <h2 className="text-lg mb-2 font-bold">Visual Management</h2>
+                    <span className="text-sm opacity-70">Manage visuals</span>
+                    <Divider sx={{ my: 1, backgroundColor: 'white', height: '1px' }} flexItem className="!my-5" />
+                    <span className="text-sm opacity-70">Last visuals :</span>
+                    <div id="last-visuals" className="flex flex-row overflow-hidden">
+                        {
+                            fiveLastVisuals === undefined || fiveLastVisuals.length === 0 ? (
+                                <p className="flex flex-col justify-center text-text h-full items-center">No visuals found.</p>
+                            ) : (
+                                fiveLastVisuals.map((visual) => (
+                                    <Card key={visual.automaton_id} sx={{ height: 'fit-content', minWidth: '200px' }} className="flex flex-col justify-between items-start !bg-background !text-text m-2 p-2">
+                                        <CardContent className="flex flex-col items-start text-left w-full">
+                                            <p className="text-lg text-text font-bold w-full">{visual.name}</p>
+                                            <p className="text-sm text-textAlt mb-2 w-full">{visual.automaton_id}</p>
+                                        </CardContent>
+                                    </Card>
+                                ))
+                            )
+                        }
+                    </div>
+                </button>
+
+                <button className="flex flex-col bg-backgroundAlt p-5 w-full h-fit 
+                    text-text rounded-lg hover:scale-105 hover:ring-4 hover:ring-primary 
+                    focus:scale-105 focus:ring-4 focus:ring-primary transition-all duration-200 cursor-pointer"
                     onClick={() => openAdminPopup("comment")}>
                     <h2 className="text-lg mb-2 font-bold">Comment Management</h2>
                     <span className="text-sm opacity-70">Manage user comments and feedback</span>
                     <Divider sx={{ my: 1, backgroundColor: 'white', height: '1px' }} flexItem className="!my-5" />
                     <span className="text-sm opacity-70">Last comments :</span>
-
-                    <div className="flex flex-col overflow-y-auto h-full w-full px-5 mt-5 gap-5">
+                    <div id="last-comments" className="flex flex-row overflow-hidden">
                         {
                             fiveLastComments === undefined || fiveLastComments.length === 0 ? (
-                                <p className="flex flex-col justify-center text-white h-full items-center">No comments found.</p>
+                                <p className="flex flex-col justify-center text-text h-full items-center">No comments found.</p>
                             ) : (
                                 fiveLastComments.map((comment) => (
-                                    <Card key={comment.id} sx={{ height: '100px' }} className="flex flex-row justify-between items-center !bg-midnight-opacity !text-white">
-                                        <CardContent className="flex flex-col items-start text-left">
-                                            <p className="text-lg font-bold w-full overflow-hidden text-ellipsis truncate">{comment.contents}</p>
-                                            <p className="text-sm opacity-70 w-full">{comment.id}</p>
+                                    <Card key={comment.id} sx={{ height: 'fit-content', minWidth: '200px' }} className="flex flex-col justify-between items-start !bg-background !text-text m-2 p-2">
+                                        <CardContent className="flex flex-col items-start text-left w-full">
+                                            <p className="text-lg text-text font-bold w-full overflow-hidden text-ellipsis truncate">{comment.contents}</p>
+                                            {/* <p className="text-sm text-textAlt mb-2 w-full">{formatDistance(new Date(comment.created_at), new Date(), { addSuffix: true })}</p> */}
+                                            <p className="text-xs text-textAlt w-full">{comment.id}</p>
                                         </CardContent>
                                     </Card>
                                 ))
@@ -153,6 +189,7 @@ export default function AdminPage() {
                         }
                     </div>
                 </button>
+
             </div>
 
             {/* ADMIN PAGE POPUP */}
