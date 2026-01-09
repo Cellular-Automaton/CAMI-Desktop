0af71a69954dd1a476373b007215ba3bd4e49dd0
Louis Bassagal <91676343+LouisBassagal@users.noreply.github.com>
2026-01-09 13:20:01 +0100

    [FIX] Delete dev tools

--- src/main.js
+++ src/main.js
@@ -31,10 +31,6 @@ const createWindow = () => {
   });
 
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
-  
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
